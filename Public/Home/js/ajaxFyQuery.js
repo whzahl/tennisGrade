fe000abf778215001d8a43cd75b23d87ffc7cd92
hangboss1761 <1779043401@qq.com>
2017-09-11 22:21:48 +0800

    gyh pc

--- Public/Home/js/ajaxFyQuery.js
+++ Public/Home/js/ajaxFyQuery.js
@@ -1,30 +1,29 @@
 // 引用在TeacherQuery/index,PlaceQuery/index,CertificateQuery/index,Live/index页面
 // ajax的分页查询
-
-var pageFy = 1;
-var maxPageFy = 1;
-var keyFy;
+$(function () {
+    var pageFy = 1;
+    var maxPageFy = 1;
+    var keyFy;
+    var $targetCityName = $("[name=city]");
+    var $targetAreaName = $("[name=area]");
+    $("[name=province]").change(function () {
+        loadData('/Home/Base/city',{code : $("[name=province] option:selected").val()}, cityQuery);
+    });
+    $targetCityName.change(function () {
+        loadData('/Home/Base/area',{code: $("[name=city] option:selected").val()},areaQuery);
+    });
+});
 
 // el:目标元素；option：ajax参数
-function loadData(el, options){
-    this.options = {
-        url:'/Home/AjaxFyQuery/',
-        type:'GET',
-        data:'',
-        successFun:'',
-        dataType:'json'
-    };
-
-    for (var i in options){
-        this.options[i] = options[i];
-    }
-
+function loadData(url, data, fun){
     $.ajax({
-        type:this.options.type,
-        url:this.options.url,
-        data:this.options.data,
-        dataType:this.options.dataType,
-        success:this.options.successFun()
+        type: 'GET',
+        url: url,
+        data: data,
+        dataType: 'json',
+        success:function (data) {
+            fun(data);
+        }
     })
 }
 
@@ -37,21 +36,20 @@ function appendChild(targetName,data,code,name) {
         targetName.append(insetOptions);
     }
 }
-var $targetCityName = $("[name=city]");
-var $targetAreaName = $("[name=area]");
 
 // 查询市
-function cityQuery() {
-    loadData($targetCityName,{
-        url:'/Home/Base/area',
-        type:'GET',
-        data:{
-            //获取被选中option的value
-            code:$("[name=province] option:selected").val()
-        }
-    })
+function cityQuery(data) {
+    var $targetName = $("[name=city]");
+    $targetName.html("<option value='市'>选择市</option>");
+    appendChild($targetName, data, 'code', 'name');
 }
 
+// 查询区
+function areaQuery(data) {
+    var $targetName = $("[name=area]");
+    $targetName.html("<option value='区'>选择区</option>");
+    appendChild($targetName, data, 'code', 'name');
+}
 // 动态添加分页组件的方法
 function appendFyDom(el) {
     
