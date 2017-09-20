c72096f448bc40f4f37bf2f992b564c2cece0f76
hangboss1761 <1779043401@qq.com>
2017-09-20 20:19:52 +0800

    gyh pc

--- Application/Home/Controller/NewsController.class.php
+++ Application/Home/Controller/NewsController.class.php
@@ -12,8 +12,28 @@ class NewsController extends BaseController{
 		parent::__construct();
 	} 
 
+	public function index(){
+	    //查询已发布的新闻，并分页显示
+        $arrWhere['status'] = 1;
 
-	
+        $intCount = D('News','Service')->count($arrWhere);
+        $Page = new \Think\Page($intCount,2);// 实例化分页类 传入总记录数和每页显示的记录数
+        $show = $Page->show();// 分页显示输出
+        $first = $Page->firstRow;
+        $list = $Page->listRows;
+        $arrData = D('News','Service')->findAll($arrWhere,$first,$list);
+        $this->count = $intCount;
+        $this->page = $show;
+        $this->list = $arrData;
+
+        $this->display();
+    }
+
+    public function content(){
+	    $arrWhere['nid'] = I('get.nid');
+	    $arrData = D('News','Service')->findOne($arrWhere);
+	    $this->list = $arrData;
+    }
 	
 	
 	
