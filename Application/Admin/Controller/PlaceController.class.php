3a8ee3542e8abbd432b313646d9058c00eecd333
hangboss1761 <1779043401@qq.com>
2017-10-17 17:48:58 +0800

    gyh pc

--- Application/Admin/Controller/PlaceController.class.php
+++ Application/Admin/Controller/PlaceController.class.php
@@ -56,18 +56,10 @@ class PlaceController extends CheckController{
             }
             $arrWhere['picture'] = implode('、',$strpicture);
     		$arrWhere['create_time'] = time();
-//    		dump($arrWhere['picture']);
-//    		dump($arrWhere);
-//    		dump($isArrImagesEmpty);
-//    		exit();
-//            dump(empty($arrImage));
-//            dump(empty($arrImages));
-//            dump(empty($arrImage)&&empty($arrImages));
 
             //不修改图片
     		if ($isArrImagesEmpty&&empty($arrImages)){
     			$strpicture = I('picture');
-                dump(1);
     			$arrWhere['picture'] = implode('、',$strpicture);
     			$arrData=D('Place','Service')->edit($arrWhere);
     		}
@@ -81,15 +73,12 @@ class PlaceController extends CheckController{
     			// 上传文件
     			$info   =    $upload->uploadOne($_FILES['certificate']);
     			if(!$info) {// 上传错误提示错误信息
-//                    dump($info);
     				$this->error($upload->getError());
 
     			}else{// 上传成功
     				$arrWhere['certificate'] = '/Public/Uploads/'.$info['savepath'].$info['savename'];
     			}
     			$strpicture = I('picture');
-//                dump($strpicture);
-//                exit();
     			$arrWhere['picture'] = implode('、',$strpicture);
     			$arrData=D('Place','Service')->edit($arrWhere);
     		}
@@ -189,6 +178,142 @@ class PlaceController extends CheckController{
     	}
     }
 
+    Public function check(){
+        if(IS_POST){
+            $arrWhere=I('post.');
+            $strpicture = I('post.picture');
+            $arrImage = array($_FILES['picture']['name']);
+            $arrImages = $_FILES['certificate']['name'];
+            $isArrImagesEmpty  = true;
+            foreach ($arrImage[0] as $key=>$value){
+                if (!empty($value)){
+                    $isArrImagesEmpty  = false;
+                }
+            }
+            $arrWhere['picture'] = implode('、',$strpicture);
+            $arrWhere['create_time'] = time();
+
+            //不修改图片
+            if ($isArrImagesEmpty&&empty($arrImages)){
+                $strpicture = I('picture');
+                $arrWhere['picture'] = implode('、',$strpicture);
+                $arrData=D('Place','Service')->edit($arrWhere);
+            }
+
+            //营业执照改变  场地照片不变
+            else if ($isArrImagesEmpty && !empty($arrImages)){
+                $upload = new \Think\Upload();// 实例化上传类
+                $upload->maxSize   =     3145728 ;// 设置附件上传大小
+                $upload->exts      =     array('jpg', 'gif', 'png', 'jpeg');// 设置附件上传类型
+                $upload->rootPath  =      './Public/Uploads/'; // 设置附件上传根目录
+                // 上传文件
+                $info   =    $upload->uploadOne($_FILES['certificate']);
+                if(!$info) {// 上传错误提示错误信息
+                    $this->error($upload->getError());
+
+                }else{// 上传成功
+                    $arrWhere['certificate'] = '/Public/Uploads/'.$info['savepath'].$info['savename'];
+                }
+                $strpicture = I('picture');
+                $arrWhere['picture'] = implode('、',$strpicture);
+                $arrData=D('Place','Service')->edit($arrWhere);
+            }
+
+            //营业执照不变  场地照片改变
+            else if (!$isArrImagesEmpty && empty($arrImages)){
+                $upload = new \Think\Upload();// 实例化上传类
+                $upload->maxSize   =     3145728 ;// 设置附件上传大小
+                $upload->exts      =     array('jpg', 'gif', 'png', 'jpeg');// 设置附件上传类型
+                $upload->rootPath  =      './Public/Uploads/'; // 设置附件上传根目录
+                // 上传文件
+                $info1   =    $upload->upload(array($_FILES['picture']));
+                $pictureUrl = array();
+                $pictureUploadUrl = array();
+
+                if(!$info1) {// 上传错误提示错误信息
+                    $this->error($upload->getError());
+
+                }else{// 上传成功
+                    foreach($info1 as $key=>$value){
+                        if(!empty($value)){
+                            $pictureUploadUrl[] = '/Public/Uploads/'.$value['savepath'].$value['savename'];
+                        }
+                    }
+                }
+
+                $pictureUrl = array_merge($strpicture, $pictureUploadUrl);
+                $arrWhere['picture'] = implode('、',$pictureUrl);
+                $arrData=D('Place','Service')->edit($arrWhere);
+            }
+
+            else if (!$isArrImagesEmpty && !empty($arrImages)){  //营业执照和场地照片都改变
+                $upload = new \Think\Upload();// 实例化上传类
+                $upload->maxSize   =     3145728 ;// 设置附件上传大小
+                $upload->exts      =     array('jpg', 'gif', 'png', 'jpeg');// 设置附件上传类型
+                $upload->rootPath  =      './Public/Uploads/'; // 设置附件上传根目录
+                // 上传文件
+                $info   =    $upload->uploadOne($_FILES['certificate']); // 上传文件
+                $info1   =    $upload->upload(array($_FILES['picture'])); // 上传多个文件
+
+                $pictureUrl = array();
+                $pictureUploadUrl = array();
+
+                if(!empty($info)){
+                    $arrWhere['certificate'] = '/Public/Uploads/'.$info['savepath'].$info['savename'];
+                }
+                if(!empty($info1)){
+                    foreach ($info1 as $k => $v){
+                        $pictureUploadUrl[] = '/Public/Uploads/'.$v['savepath'].$v['savename']; //每个上传路径重新组合成数组
+
+                    }
+                    $pictureUrl = array_merge($strpicture, $pictureUploadUrl);
+                    $arrWhere['picture'] = implode('、',$pictureUrl);
+                }
+
+                $arrData=D('Place','Service')->edit($arrWhere);
+            }
+            if ($arrData){
+                $this->success('修改成功','/Admin/Place/index');
+            }else {
+                $this->error('修改失败');
+            }
+        }
+        else{
+            $province = M('tg_province')->select();
+            $this->province = $province;
+            $arrWhere['pid']=I('get.pid');
+            $arrData=D('Place','Service')->findOne($arrWhere);
+            //获取省市区的code,根据code从数据库中查询他们的名字,放在arrData中返回
+
+            //查询字段code
+            $provinceCode['code'] = $arrData['province'];
+            $cityCode['code'] = $arrData['city'];
+            $areaCode['code'] = $arrData['area'];
+
+            //查询结果
+            $provinceData = (D('Province','Service')->findOne($provinceCode));
+            $areaData = D('Area','Service')->findOne($areaCode);
+            $cityData = D('City','Service')->findOne($cityCode);
+//            结果存入数组arrData
+            $arrData['provinceName'] = $provinceData['name'];
+            $arrData['areaName'] = $areaData['name'];
+            $arrData['cityName'] = $cityData['name'];
+
+//            查询province code 为$provinceCode['code']的city
+//            查询citycode 为$cityCode['code']的city
+            $arrWhereCity['provincecode'] = $provinceCode['code'];
+            $arrWhereArea['citycode'] = $cityCode['code'];
+            $arrCity = D('City','Service')->findAll($arrWhereCity);
+            $arrArea = D('Area','Service')->findAll($arrWhereArea);
+            $arrPicture = explode('、', $arrData['picture']);
+            $this->list=$arrData;
+            $this-> list1 = $arrPicture;
+            $this->arrCity = $arrCity;
+            $this->arrArea = $arrArea;
+            $this->display();
+        }
+    }
+
     public function delete(){
     	$arrWhere['pid'] = I('get.pid');
     	$arrData = D('Place','Service') ->delete($arrWhere);
@@ -198,7 +323,9 @@ class PlaceController extends CheckController{
     		$this->error('删除失败');
     	}
     }
-    
+
+
+
     public function add(){
     	$this->display();
     }
